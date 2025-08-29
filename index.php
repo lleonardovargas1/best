30c7deb8fb2e599dba1c0fe608b58c73e129716f
lleonardovargas1 <somostricolor90@gmail.com>
2025-08-29 09:09:20 -0300

    0.1

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width='device-width', initial-scale=1.0">
+    <title>Document</title>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header class="vermelho">
+    <input type="text" id="fname" name="fname"><br>
+  </header>  
+</body>
+</html>
